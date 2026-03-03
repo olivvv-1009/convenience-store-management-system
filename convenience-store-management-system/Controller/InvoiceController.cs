990337332799bbb87f5f0b3bc0d90d6356b0aab7
Thu <thudinh.31241026994@st.ueh.edu.vn>
2026-03-03 09:59:19 +0700

    Fix project folder structure

---
+++ convenience-store-management-system/Controller/InvoiceController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convenience_store_management_system.Controller
+{
+    internal class InvoiceController
+    {
+    }
+}
